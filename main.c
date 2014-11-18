71af51b026b7d66362f552e54a63fbf1d593c0b7
Steven Blair <sblair42@siu.edu>
2014-11-17 21:58:24 -0600

    Modified main to compute fault-free and faulty response for all input vectors and to determine if a fault will be detected. Note that functionality needs to be verified.

--- main.c
+++ main.c
@@ -22,6 +22,9 @@ NODE graph[Mnod];
 //structure used to store the input vectors information in .vec file
 PATTERN vector[Mpt];
 
+//structure used to store the input vectors information in .vec file
+FAULT fault[Mft];
+
 //random variables
 int a,b,c,d;
 
@@ -63,6 +66,27 @@ printf("\nIndex\tInputVector\n");
 for(a=0;a<Total;a++){  printf("%d\t%s",a,vector[a].piv); }
 
 /*************************************************************************************************
+ Read the .faults file and store the information in faults structure
+*************************************************************************************************/
+
+//file pointer to open .vec file
+ffau=fopen(argv[5],"r");
+
+//read .vec file and store in vector structure and return Total number of patterns
+Tfs=0; Tfs=ReadFaults(ffau,fault);
+
+//close file pointer for .vec file
+fclose(ffau);
+
+//print total number of patterns in .vec file
+printf("\nTotal No. of Faults: %d",Tfs);
+
+//print all members of vector structure
+printf("\n\nStuck-At Faults");
+printf("\nNode\tStuck-At\n");
+for(a=0;a<Tfs;a++){  printf("%i\t%i\n",fault[a].Snod,fault[a].Sval); }
+
+/*************************************************************************************************
  Set XVal in vector structure
 *************************************************************************************************/
 
@@ -93,35 +117,54 @@ char input_vector[Mpi];
 char output_vector[Mpo];
 int output_vector_length = 0;
 
-printf("\nOutput Vectors");
-printf("\nIndex\tInputVector\tOutputVector\n");
+//Create structures to hold data to be printed to screen/file
+char fault_string[Mft+1][Mlin];
+char output_vector_string[Mft+1][Mpo];
+int detected_int[Mft+1];
 
-fprintf(fres,"Index\tInputVector\tOutputVector\n");
+//Flag to store whether a fault can be sensed or not
+int detected_flag;
 
-int i = 0; int j = 0; Stack* temp;
+int i = 0; int j = 0; int m = 0; Stack* temp;
 for(i = 0; i < Total; i++) //Iterate over each input vector
 {
+    //Reset values
+    memset(fault_string, 0, (Mft+1)*Mlin);
+    memset(output_vector_string, 0, (Mft+1)*Mpo);
+    memset(detected_int, -1, sizeof(detected_int[0])*(Mft+1));
+    detected_flag = 0;
 
     /**************************************************************************
      Simulation
     **************************************************************************/
 
-    //Make a copy of the input vector and topographically sorted stack;
-    //Then apply the input vector and get the output vector
-    temp = copy_stack(topo_sorted);
-    memcpy(input_vector, vector[i].piv, Mpi);
-    output_vector_length = apply_vector(graph, Max, temp, input_vector, output_vector);
-    delete_stack(temp);
+    for(m = 0; m < Tfs; m++) // Iterate over each fault
+    {
+        temp = copy_stack(topo_sorted);
+        memcpy(input_vector, vector[i].piv, Mpi);
 
+        if(m==0)
+        {
+            sprintf(fault_string[m], "None");
+            output_vector_length = apply_vector_wofault(graph, Max, temp, input_vector, output_vector_string[m]);
+            detected_int[m] = 2;
+        }
+        else
+        {
+            sprintf(fault_string[m],"%i/%i",fault[m-1].Snod,fault[m-1].Sval);
+            apply_vector_wfault(graph, Max, temp, input_vector, output_vector_string[m], fault[m-1].Snod, fault[m-1].Sval);
+            detected_int[m] = compare_faulty_circuit_outputs(graph, Max, fault[m-1].Snod, fault[m-1].Sval);
+        }
+
+        delete_stack(temp);
+    }
 
     /**************************************************************************
      Log
     **************************************************************************/
 
-    //Print index
-    fprintf(fres,"%d\t",i);
-    printf("%d\t",i);
-
+    printf("\nTest Pattern: ");
+    fprintf(fres, "\nTest Pattern: ");
     //Print input vector
     for(j = 0; j < Mpi; j++)
     {
@@ -131,25 +174,49 @@ for(i = 0; i < Total; i++) //Iterate over each input vector
         }
         else
         {
-            fprintf(fres,"%c",input_vector[j]);
             printf("%c", input_vector[j]);
+            fprintf(fres, "%c", input_vector[j]);
         }
     }
 
-    //Print seperator
-    fprintf(fres,"\t");
-    printf("\t");
+    //Print table header
+    printf("\nFault\tOutput\tDetected\n");
+    fprintf(fres, "\nFault\tOutput\tDetected\n");
 
-    //Print output vector
-    for(j = 0; j < output_vector_length; j++)
+    //Print table rows
+    for(m = 0; m < Tfs; m++)
     {
-        fprintf(fres,"%c",output_vector[j]);
-        printf("%c",output_vector[j]);
+        if(detected_int[m] == 2)
+        {
+            printf("%s\t%s\tNA\n", fault_string[m], output_vector_string[m]);
+            fprintf(fres, "%s\t%s\tNA\n", fault_string[m], output_vector_string[m]);
+        }
+        else if(detected_int[m] == 0 || detected_int[m] == 1)
+        {
+            printf("%s\t%s\t%i\n", fault_string[m], output_vector_string[m], detected_int[m]);
+            fprintf(fres, "%s\t%s\t%i\n", fault_string[m], output_vector_string[m], detected_int[m]);
+
+            if(detected_int[m] == 1)
+                detected_flag = 1;
+        }
+        else
+        {
+            printf("%s\t%s\tER\n", fault_string[m], output_vector_string[m]);
+            fprintf(fres, "%s\t%s\tER\n", fault_string[m], output_vector_string[m]);
+        }
     }
 
-    //Print newline
-    fprintf(fres, "\n");
-    printf("\n");
+    //Print detection status
+    if(!detected_flag)
+    {
+        printf("STATUS: FAULT NOT DETECED\n");
+        fprintf(fres, "STATUS: FAULT NOT DETECED\n");
+    }
+    else
+    {
+        printf("STATUS: FAULT DETECTED\n");
+        fprintf(fres, "STATUS: FAULT DETECTED\n");
+    }
 }
 
 //Free stack used to hold topological sort
